5079485056b9a6e27ad7ffa6ef9dc947cb1c961f
thelaurelyy <3246997955@qq.com>
2018-07-16 13:49:08 +0800

    第一讲 vue-router的简单变更及疑问

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ vue-router 学习笔记
 缘由：vue制作的都是单页应用，只有一个主要的index.html页面，所以无法用a标签链接跳转，只能用vue-router进行管理。
 
 第一讲 vue-router的入门
+    在引用导航链接时，语法为： <router-link to="/">[显示字段]</router-link>
+to：是我们的导航路径，需要填写的是在router/index.js文件里配置的path值，如果要导航到默认的首页只需要写成"/"
+显示字段：就是我们要展示给用户的导航标签名称，例如：首页、产品展示等等
+
+    Q1：组件 Hi.vue 和路由 index.js 文件中的name属性的用处是什么呢？
+    Q2：注意 router-link标签 和 router-view标签 的区别
--- vueRouterLearning/src/router/index.js
+++ vueRouterLearning/src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Hi from '@/components/Hi'
 
-Vue.use(Router)
+
+Vue.use(Router)                 //vue 全局使用Router
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+routes: [                       //配置路由，这里是数组的形式
+    {                            //每一个链接都是一个对象
+      path: '/',                 //连接路径
+      name: 'HelloWorld',        //路由名称
+      component: HelloWorld     //对应的组件模板
+    },{
+      path: '/hi',
+      name: 'Hi',
+      component: Hi
     }
   ]
 })
